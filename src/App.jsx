ad6762722186e34e41cae060d7959b64dda10db2
Simon-Sahlin <simonsahlin@somin.se>
2024-06-21 00:53:55 +0200

    Add dummy nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./Navbar.jsx"
+// import Itemcard from "./Itemcard.jsx"
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Hello World</h1>
     </>
   )
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li><em>Store</em></li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar;
